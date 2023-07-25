sharding revision for the correct computation

colo_inference.py
@@ -65,9 +65,11 @@ def inference():
   model_args, inference_args = parser.parse_args_into_dataclasses()
   logger = get_dist_logger()
   shard_pg = ProcessGroup(tp_degree=tp_degree)
-  default_dist_spec = ShardSpec([dims], [tp_degree])
+  embedding_dist_spec = ShardSpec([-1], [tp_degree])
+  linear_dist_spec = ShardSpec([0], [tp_degree])
         
-  with ColoInitContext(device=get_current_device(), default_dist_spec=default_dist_spec, default_pg=shard_pg,
+  with ColoInitContext(device=get_current_device(), embedding_dist_spec=embedding_dist_spec, 
+                       linear_dist_spec=linear_dist_spec, default_pg=shard_pg,
                        model_name=model_args.model_name_or_path):
     model_config = AutoConfig.from_pretrained(model_args.model_name_or_path)
     if 'llama-7b' in model_args.model_name_or_path:
@@ -117,7 +119,7 @@ def inference():
   if inference_args.override_checkpoint is not None:
     logger.info("Loading override checkpoint.", ranks=[0])
     try:
-      state_dict = torch.load(inference_args.override_checkpoint + '/shard_' + str(dist.get_rank()) + '.pt')
+      state_dict = torch.load(inference_args.override_checkpoint + 'shard_' + str(dist.get_rank()) + '.pt')
       model.load_state_dict(state_dict)
     except:
       raise Exception("Failed to load checkpoint")

colossalai/zero/gemini/colo_init_context.py
@@ -29,8 +29,7 @@ def _convert_to_coloparam(param: torch.nn.Parameter,
                           device: torch.device,
                           dtype=torch.float,
                           default_pg: Optional[ProcessGroup] = None,
-                          default_dist_spec: Optional[Any] = None,
-                          not_default_dist_spec=False) -> ColoParameter:
+                          default_dist_spec: Optional[Any] = None) -> ColoParameter:
 
     if type(param) is ColoParameter:
         return param
@@ -48,15 +47,14 @@ def _convert_to_coloparam(param: torch.nn.Parameter,
     # NOTE() embedding usually can not be correctly sharded. So I use except to handle
     # the param that can not be sharded by the default plan
 
-    if not not_default_dist_spec:
-        if default_pg is not None:
-            colo_param.set_process_group(default_pg)
+    if default_pg is not None:
+        colo_param.set_process_group(default_pg)
 
-        if default_dist_spec is not None:
-            try:
-                colo_param.set_dist_spec(default_dist_spec)
-            except:
-                pass
+    if default_dist_spec is not None:
+        try:
+            colo_param.set_dist_spec(default_dist_spec)
+        except:
+            pass
     return colo_param
 
 
@@ -74,14 +72,17 @@ class ColoInitContext(InsertPostInitMethodToModuleSubClasses):
                  device: torch.device = torch.device('cpu'),
                  dtype: torch.dtype = torch.float,
                  default_pg: Optional[ProcessGroup] = None,
-                 default_dist_spec=None,
+                 embedding_dist_spec=None,
+                 linear_dist_spec=None,
                  model_name='llama-7b'):
         """
         Args:
             device (torch.device): the device where parameters initialized are resident. Defaults to torch.device('cpu').
             dtype (torch.dtype): the dtype of parameters initialized. Defaults to torch.float.
             default_pg (ProcessGroup): the default process group for all initialized parameters.
-            default_dist_spec: the default distributed specifications.
+            embedding_dist_spec: the default distributed specifications of Embedding tensor.
+            linear_dist_spec: the default distributed specifications of Linear tensor.
+            model_name: The partial model name for training/fine-tuning/inference.
         """
         super().__init__()
         self._device = device
@@ -89,7 +90,8 @@ class ColoInitContext(InsertPostInitMethodToModuleSubClasses):
 
         self._register_colo_modules(model_name=model_name)
         self._default_pg = default_pg
-        self._default_dist_spec = default_dist_spec
+        self._embedding_dist_spec = embedding_dist_spec
+        self._linear_dist_spec = linear_dist_spec
 
     def _register_colo_modules(self, model_name):
         from colossalai.nn.parallel.layers import ColoEmbedding, ColoLinear, register_colo_module
@@ -128,14 +130,15 @@ class ColoInitContext(InsertPostInitMethodToModuleSubClasses):
                 colo_param = replaced_tensors[param]
             else:
                 if param_name == 'bias':
+                    colo_param = _convert_to_coloparam(param, self._device, self._dtype)
+                elif 'Embedding' in str(submodule.named_parameters):
                     colo_param = _convert_to_coloparam(param, self._device, self._dtype, self._default_pg,
-                                                    self._default_dist_spec, not_default_dist_spec=True)
-                elif 'Linear' in str(submodule.named_parameters) or 'Embedding' in str(submodule.named_parameters):
+                                                    self._embedding_dist_spec)
+                elif 'Linear' in str(submodule.named_parameters):
                     colo_param = _convert_to_coloparam(param, self._device, self._dtype, self._default_pg,
-                                                    self._default_dist_spec)
+                                                    self._linear_dist_spec)
                 else:
-                    colo_param = _convert_to_coloparam(param, self._device, self._dtype, self._default_pg,
-                                                    self._default_dist_spec, not_default_dist_spec=True)
+                    colo_param = _convert_to_coloparam(param, self._device, self._dtype)
                 replaced_tensors[param] = colo_param
       
             delattr(submodule, param_name)

train_colo.py
@@ -66,7 +66,7 @@ def train_epoch(epoch, model, optimizer, lr_scheduler, dataloader, booster, coor
         for batch in pbar: # Iters - 2 GPUs: 13000(=52000/(2*2)), 4 GPUs: 3250(=52000/(4*4)), 8 GPUs: 812
             # Forward
             optimizer.zero_grad()
-            batch = move_to_cuda(batch, torch.cuda.current_device())           
+            batch = move_to_cuda(batch, torch.cuda.current_device())         
             outputs = model(use_cache=False, **batch)
             loss = outputs['loss']
             # Backward
@@ -74,8 +74,8 @@ def train_epoch(epoch, model, optimizer, lr_scheduler, dataloader, booster, coor
             optimizer.step()
             lr_scheduler.step()
             # Print batch loss
-            pbar.set_postfix({'loss': loss.item(), 'Memory usage': GPUtil.getGPUs()[0].memoryUsed})
-            # pbar.set_postfix({'loss': loss.item()}) 
+            # pbar.set_postfix({'loss': loss.item(), 'Memory usage': GPUtil.getGPUs()[0].memoryUsed})
+            pbar.set_postfix({'loss': loss.item()}) 
             losses.append(loss.item())
     
     print_rank_0('Average loss of epoch {0}: {1:.2f}, Memory usage: {2}'.format(epoch + 1, mean(losses), 
@@ -229,7 +229,7 @@ def get_size(bytes, suffix="B"):
 def train():
     tp_degree=8
     dp_degree=1
-    dims=-1 # 0: by row (bs=8, peak_mem=28487 MB), -1: by col (bs=8, peak_mem=24855 MB)
+    # dims=0 # 0: by row (bs=8, peak_mem=28487 MB), -1: by col (bs=8, peak_mem=24855 MB)
     # Launch ColossalAI
     # colossalai.launch_from_torch(config={}, seed=0)
     colossalai.launch_from_torch(config=dict(parallel=dict(data=dp_degree, pipeline=1, 
@@ -246,9 +246,11 @@ def train():
     # with PipelinableContext():
 
     shard_pg = ProcessGroup(tp_degree=tp_degree)
-    default_dist_spec = ShardSpec([dims], [tp_degree])
+    embedding_dist_spec = ShardSpec([-1], [tp_degree])
+    linear_dist_spec = ShardSpec([0], [tp_degree])
         
-    with ColoInitContext(device=get_current_device(), default_dist_spec=default_dist_spec, default_pg=shard_pg,
+    with ColoInitContext(device=get_current_device(), embedding_dist_spec=embedding_dist_spec, 
+                         linear_dist_spec=linear_dist_spec, default_pg=shard_pg,
                          model_name=model_args.model_name_or_path):
     # with ColoInitContext(device=get_current_device()):
         print_rank_0('[0]Used GPU mem: {0}'.format(GPUtil.getGPUs()[0].memoryUsed))  # 1421 MB
@@ -297,17 +299,16 @@ def train():
             n = n.replace('model.', '')
         x = state_dict[n]
         if not 'norm' in n and not 'bias' in n:
-            x = x.chunk(tp_degree, dim=dims)
-            x = x[dist.get_rank() % tp_degree]
+            if 'embed' in n:
+                x = x.chunk(tp_degree, dim=-1)
+                x = x[dist.get_rank() % tp_degree]
+            else:
+                x = x.chunk(tp_degree, dim=0)
+                x = x[dist.get_rank() % tp_degree]
         p.data.copy_(x)
     
     print_rank_0('[1]Used GPU mem: {0}'.format(GPUtil.getGPUs()[0].memoryUsed)) # 27189 MB // When sharding in with ColoInitContext 5545 MB
     print_rank_0('[1]Virtual used mem: {0}'.format(get_size(psutil.virtual_memory().used))) # 14.61 GB
-
-    # for p in model.parameters():
-    #     print(p)
-    #     print(p.data.size())
-    #     sys.exit()
 
     compute_spec = ComputeSpec(ComputePattern.TP1D)
     init_colo_module(model, compute_spec, pg=shard_pg, recursive=True)
@@ -357,7 +358,7 @@ def train():
 
     # Finish training and evaluate
     logger.info(f"Finish finetuning", ranks=[0])
-    output_dir = './trained/shard_colo_opt-6.7b/shard_' + str(dist.get_rank()) + '.pt'
+    output_dir = './trained/shard_colo_llama-7b/shard_' + str(dist.get_rank()) + '.pt'
     booster.save_model(model, output_dir, tp_degree=tp_degree)
     logger.info(f"Saving model checkpoint to {output_dir}")
 